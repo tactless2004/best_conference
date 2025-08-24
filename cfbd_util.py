dc206f6de804471f0ab5cecf0bc95043eba8810b
tactless2004 <leyton.mckinney@gmail.com>
2025-08-24 17:41:12 -0500

    Implement scoring functionality

--- cfbd_util.py
+++ cfbd_util.py
@@ -3,6 +3,14 @@ import os
 from typing import Any
 import cfbd
 
+# Global constantst
+conference_name_map_file = {
+    "Big Ten" : "Big_Ten",
+    "Big 12" : "Big_12",
+    "ACC" : "ACC",
+    "SEC" : "SEC"
+}
+
 def get_records(year: int) -> None:
     configuration = cfbd.Configuration(
         access_token = os.getenv("API_KEY")
@@ -102,13 +110,8 @@ def compute_points_for_winner(loser_rank: float, winner_rank: float) -> float:
     return loser_rank/winner_rank
 
 def write_games_data(team, conference) -> None:
-    cf_map = {
-        "Big Ten" : "Big_Ten",
-        "Big 12" : "Big_12",
-        "ACC" : "ACC",
-        "SEC" : "SEC"
-    }
-    write_response(f"game_stats/{cf_map[conference]}_{team}_gamedata.txt", get_games(2024, team))
+    '''write_response() and get_games() simplified and makes consistent names.'''
+    write_response(f"game_stats/{conference_name_map_file[conference]}_{team}_gamedata.txt", get_games(2024, team))
 
 def write_all_games_data(conferences: list):
     with open("conference_team_ranks.json", "r", encoding = "utf-8") as f:
@@ -118,8 +121,83 @@ def write_all_games_data(conferences: list):
         assert isinstance(conference_rank_data[conference], dict)
         for team in conference_rank_data[conference].keys():
             write_games_data(team, conference)
+def score_game(team: str, game_data: str, opponent_conferences: list) -> float:
+    '''Score individual game for an individual'''
+    # DRY (Don't repeat yourself)
+    def grab_field(text: str, start_str):
+        '''
+        Grab all the text after the start string then look for the first element enclosed in single quotes
+        '''
+        text = text.partition(start_str)[2]
+        start = text.find("'")
+        end = text.find("'", start + 1)
+        return text[start + 1:end]
 
-def process_games(team, team_rank, team_conference) -> float:
+    def grab_points(text: str):
+        home_points = int(text.partition("home_points=")[2].split(" ")[0])
+        away_points = int(text.partition("away_points=")[2].split(" ")[0])
+        return home_points, away_points
+
+    # Rank data for use in scoring (if needed)
+    with open("conference_team_ranks.json", "r", encoding = "utf-8") as f:
+        rank_data = json.load(f)
+
+    # Parse the game_data for all relevant fields
+    home_team = grab_field(game_data, "home_team=")
+    away_team = grab_field(game_data, "away_team=")
+    home_conference = grab_field(game_data, "home_conference=")
+    away_conference = grab_field(game_data, "away_conference=")
+    home_points, away_points = grab_points(game_data)
+
+    if team == home_team:
+        # If the opponent team is not an opposing P4 Conference, we will not consider this game.
+        if away_conference not in opponent_conferences:
+            return 0.0
+
+        # If team won
+        if home_points > away_points:
+            return compute_points_for_winner(
+                loser_rank = float(rank_data[away_conference][away_team]),
+                winner_rank = float(rank_data[home_conference][home_team])
+            )
+
+    elif team == away_team:
+        # If the opponent team is not an opposing P4 Conference, we will not consider this game.
+        if home_conference not in opponent_conferences:
+            return 0.0
+
+        # If team won
+        if away_points > home_points:
+            return compute_points_for_winner(
+                winner_rank = float(rank_data[away_conference][away_team]),
+                loser_rank = float(rank_data[home_conference][home_team])
+            )
+    else:
+        raise RuntimeError(f"provided team, {team} was not found in team data")
+    return 0.0
+def process_games(team, team_conference) -> float:
     '''
     Iterate overall games played by a team and compute the points earned for interconference play (if any).
     '''
+    # This should be done in a different way in future work, reading the whole json, to use one line in it
+    # over and over again is poor form.
+    total_score = 0.0
+
+    opponent_conferences = list(
+        set(
+            ["ACC", "SEC", "Big 12", "Big Ten"]).difference(
+                set([team_conference])
+        )
+    )
+
+    with open(
+        file = f"game_stats/{conference_name_map_file[team_conference]}_{team}_gamedata.txt",
+        mode = "r",
+        encoding = "utf-8"
+    ) as f:
+        games_data = f.readlines()
+
+    for game_data in games_data:
+        total_score += score_game(team, game_data, opponent_conferences)
+
+    return total_score
